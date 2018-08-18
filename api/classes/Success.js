d794f6ff15e7c8dd709aaccf35757c0d2444e864
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-08-18 14:26:50 +1000

    Success response class

---
+++ api/classes/Success.js
@@ -0,0 +1,21 @@
+class Success {
+    constructor(msg = 'Request has been successfully completed') {
+        this._status = 200
+        this._msg = msg
+    }
+    
+    set status(s) {
+        this._status = s
+    }
+    
+    dispatch(res, data) {
+        let json = Object.assign(data, {
+            status: this._status,
+            message: this._msg
+        })
+
+        res.status(this._status).json(json)
+    }
+}
+
+module.exports = Success
