ea4ae747e64ce20e47b371ab7567bcdbc1ea086d
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-08-19 17:00:38 +1000

    Basic Nav and Placeholder About page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Header extends React.PureComponent {
     render() {
         return (
             <header>
                 Lyrical Tune Nav
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
             </header>
         )
     }
---
+++ frontend/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class About extends React.PureComponent {
+    render() {
+        return (
+            <main>
+                Please don't sue us! We are poor uni students
+            </main>
+        )
+    }
+}
