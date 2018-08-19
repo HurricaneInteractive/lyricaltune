66a0a05424d82685b79bf5d382d34843b30420b2
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-08-19 21:28:55 +1000

    Form Component

---
+++ frontend/src/components/Form/Form.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import Input from './InputTypes/Input'
+import NumberInput from './InputTypes/Number'
+
+export default class Form extends Component {
+    constructor() {
+        super()
+        this.renderFormContents = this.renderFormContents.bind(this)
+    }
+
+    renderFormContents(fields) {
+        const fieldTypes = {
+            text: Input,
+            number: NumberInput
+        }
+
+        return fields.map((field, i) => {
+            const InputType = fieldTypes[field.type]
+            return (
+                <InputType
+                    key={i}
+                    onChange={(e) => this.props.onChange(e)}
+                    {...field}
+                />
+            )
+        })
+    }
+
+    render() {
+        return (
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    this.props.onSubmit()
+                }}
+            >
+                { this.props.children }
+                { this.renderFormContents(this.props.fields) }
+                <div className="form-actions">
+                    <button type="submit" className="btn">{this.props.submitButtonText}</button>
+                </div>
+            </form>
+        )
+    }
+}
+
+Form.propTypes = {
+    fields: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onChange: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    submitButtonText: PropTypes.string
+}
+
+Form.defaultProps = {
+    submitButtonText: 'Submit'
+}
