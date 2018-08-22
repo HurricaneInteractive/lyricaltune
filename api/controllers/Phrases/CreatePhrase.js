6f8a900ec14325581ff503eed1133e7314b263e8
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-08-22 18:04:15 +1000

    Check if user exists before creating

--- api/controllers/Phrases/CreatePhrase.js
+++ api/controllers/Phrases/CreatePhrase.js
@@ -1,18 +1,29 @@
+const User = require('../../models/User')
 const Phrase = require('../../models/Phrases')
 const ResponseHelper = require('../../helpers/ResponseHelper')
 
 module.exports = (req, res, next) => {
     let current_user = req.jwt_user
-    const body = {...req.body}
-    body.author = current_user._id
 
-    const phrase = new Phrase(body)
+    User.findOne({ _id: current_user._id }).exec()
+        .then(user => {
 
-    phrase.save()
-        .then(createdPhrase => {
-            Phrase.find({ author: current_user._id }).exec()
-                .then(response => {
-                    ResponseHelper.phraseSavedSuccessfully(res, createdPhrase.name, response)
+            if (user === null) {
+                ResponseHelper.noUserWithId(current_user._id, next);
+            }
+
+            const body = {...req.body}
+            body.author = user._id
+        
+            const phrase = new Phrase(body)
+        
+            phrase.save()
+                .then(createdPhrase => {
+                    Phrase.find({ author: user._id }).exec()
+                        .then(response => {
+                            ResponseHelper.phraseSavedSuccessfully(res, createdPhrase.name, response)
+                        })
+                        .catch(e => ResponseHelper.returnedError(res, e))
                 })
                 .catch(e => ResponseHelper.returnedError(res, e))
         })
