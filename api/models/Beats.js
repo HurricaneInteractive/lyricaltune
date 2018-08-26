849c15027b827f42fa101b98faa58a532e8f42f3
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-08-26 15:06:09 +1000

    Beat Schema structure

--- api/models/Beats.js
+++ api/models/Beats.js
@@ -6,7 +6,21 @@ const beatsSchema = new Schema({
     _id: {
         type: Schema.Types.ObjectId,
         default: mongoose.Types.ObjectId
-    }
+    },
+    name: {
+        type: String,
+        required: true,
+        index: true
+    },
+    effects: {
+        type: Map,
+        of: Schema.Types.Mixed
+    },
+    beat: {
+        type: Map,
+        of: Object
+    },
+    author: Schema.Types.ObjectId
 }, {
     timestamps: true,
     strict: true
