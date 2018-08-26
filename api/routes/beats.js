1efb754c9dffad921adfdbe1ace146f142ed5e80
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-08-26 15:05:47 +1000

    Securing routes

--- api/routes/beats.js
+++ api/routes/beats.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const BeatsController = require('../controllers/BeatsController')
+const CheckAuthentication = require('../middleware/CheckAuthentication')
 
 // POST
-router.post('/create', BeatsController.createBeats)
+router.post('/create', CheckAuthentication, BeatsController.createBeats)
 
 // GET
-router.get('/:id', BeatsController.getBeats)
+router.get('/:id', CheckAuthentication, BeatsController.getBeats)
 
 // DELETE
-router.delete('/delete', BeatsController.deleteBeats)
+router.delete('/delete', CheckAuthentication, BeatsController.deleteBeats)
 
 // PATCH
-router.patch('/update/:id', BeatsController.updateBeats)
+router.patch('/update/:id', CheckAuthentication, BeatsController.updateBeats)
 
 module.exports = router
