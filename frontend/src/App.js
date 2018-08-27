00f07e72ce59edc8d878342678f4e94760f4e174
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-08-27 17:23:54 +1000

    Display error popup if present

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import About from './pages/About'
 import SecondChanceAuth from './components/Auth/SecondChanceAuth'
+import ErrorPopup from './components/ErrorPopup'
 
 import './App.css';
 
@@ -16,15 +17,6 @@ import './App.css';
 @inject('GlobalStore')
 @observer
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			username: '',
-			number_counter: "0",
-			select: ''
-		}
-	}
-
 	componentDidMount() {
 		this.props.UserStore.getCurrentUser()
 	}
@@ -37,15 +29,32 @@ class App extends Component {
 
 	render() {
 		const { UserStore, GlobalStore } = this.props;
+		if (GlobalStore.response_error !== null) {
+			console.log('Global Error', GlobalStore.response_error.message)
+		}
 		return (
 			<Router>
 				<div className="App">
-					{ GlobalStore.second_chance !== false ? <SecondChanceAuth UserStore={UserStore} GlobalStore={GlobalStore} second_chance={GlobalStore.second_chance} /> : '' }
+					{ 
+						GlobalStore.second_chance !== false ? (
+							<SecondChanceAuth UserStore={UserStore} GlobalStore={GlobalStore} second_chance={GlobalStore.second_chance} /> 
+						) : ('')
+					}
+					{
+						GlobalStore.response_error !== null ? (
+							<ErrorPopup errors={GlobalStore.errors} />
+						) : ('')
+					}
 					<Header />
 					<p>Authenticated: { UserStore.current_user === null || typeof UserStore.current_user === 'undefined' ? 'No' : 'Yes' }</p>
 					
 					<Route exact path="/" component={Home} />
 					<Route path="/about" component={About} />
+
+					<button onClick={(e) => {
+						e.preventDefault();
+						UserStore.followUser('5b7816566903fd1b5dbd7a95')
+					}}>Follow 123</button>
 				</div>
 			</Router>
 		);
