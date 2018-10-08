7e43a0b2508ac9d4559ee5fce0cb61f7a769f571
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-08 15:37:31 +1100

    Config file for menu items to make global link changes simpler

---
+++ frontend/src/helpers/config-menus.js
@@ -0,0 +1,22 @@
+export const headerMenu = [
+    {
+        to: '/discover',
+        name: 'Discover'
+    },
+    {
+        to: '/create',
+        name: 'Create'
+    }
+]
+
+export const footerMenu = [
+    ...headerMenu,
+    {
+        to: '/about',
+        name: 'About'
+    },
+    {
+        to: '/legalities',
+        name: 'Legalities'
+    }
+]
