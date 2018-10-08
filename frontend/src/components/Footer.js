04fea23bce321071fa1374b0ebdc49ce7827e233
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-08 15:38:57 +1100

    Footer component and styling

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import LoginSignup from './Navigation/LoginSignup'
+import LoggedIn from './Navigation/LoggedIn'
+import MenuItem from './Navigation/MenuItem'
+import { footerMenu } from '../helpers/config-menus'
+import SocialMedia from './Branding/SocialMedia'
+
+export default class Footer extends PureComponent {
+    render() {
+        return (
+            <footer>
+                <div className="container">
+                    <div className="col menu-container">
+                        <div className="user-menu">
+                            <ul>
+                            {
+                                this.props.authenticated ? (
+                                    <LoggedIn UserStore={this.props.UserStore} />
+                                ) : (
+                                    <LoginSignup UserStore={this.props.UserStore} />
+                                )
+                            }
+                            </ul>
+                            <SocialMedia />
+                        </div>
+                        <div className="main-menu">
+                            <ul>
+                                { footerMenu.map((item, key) => <MenuItem key={key} to={item.to} name={item.name} />) }
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col credit text--right">
+                        <p>Built by <a href="http://www.google.com.au" target="_blank" rel="noopener noreferrer">Sunset Studios</a></p>
+                    </div>
+                </div>
+            </footer>
+        )
+    }
+}
+
+Footer.propTypes = {
+    authenticated: PropTypes.bool,
+    UserStore: PropTypes.any.isRequired
+}
+
+Footer.defaultProps = {
+    authenticated: false
+}
