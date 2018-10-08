5c54fcf9d5cca856bc74feec2c4c354a329eb9f9
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-08 15:38:12 +1100

    MainMenu uses config and new MenuItem component

--- frontend/src/components/Navigation/MainMenu.js
+++ frontend/src/components/Navigation/MainMenu.js
@@ -1,32 +1,16 @@
 import React, { PureComponent } from 'react'
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import LoginSignup from './LoginSignup'
 import LoggedIn from './LoggedIn'
-
-const menu = [
-    {
-        to: '/discover',
-        name: 'Discover'
-    },
-    {
-        to: '/create',
-        name: 'Create'
-    }
-]
+import { headerMenu } from '../../helpers/config-menus'
+import MenuItem from './MenuItem'
 
 export default class MainMenu extends PureComponent {
     render() {
         return (
             <nav id="main-menu">
                 <ul className="menu clearfix">
-                    {
-                        menu.map((item, key) => (
-                            <li key={key}>
-                                <Link to={item.to} title={item.name}>{item.name}</Link>
-                            </li>
-                        ))
-                    }
+                    { headerMenu.map((item, key) => <MenuItem key={key} to={item.to} name={item.name} />) }
                     {
                         this.props.authenticated ? (
                             <LoggedIn UserStore={this.props.UserStore} />
