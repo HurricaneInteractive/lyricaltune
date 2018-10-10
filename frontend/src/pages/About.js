8b8f3d64f5facbabed68177fc33fbc73ca917052
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-10 20:24:49 +1100

    Updated About page structure

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import PageWrapper from '../components/Pages/PageWrapper'
+import PageTitle from '../components/Pages/PageTitle'
+
 export default class About extends React.PureComponent {
     render() {
         return (
-            <main>
+            <PageWrapper>
+                <PageTitle title="About Lyrical" />
                 Please don't sue us! We are poor uni students
-            </main>
+            </PageWrapper>
         )
     }
 }
