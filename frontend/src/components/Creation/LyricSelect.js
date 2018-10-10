00b2957df46fe1ad73222feed1a00c540482de4c
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-10 21:24:47 +1100

    Render pills / lyrics reflect store data

--- frontend/src/components/Creation/LyricSelect.js
+++ frontend/src/components/Creation/LyricSelect.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react'
+import { X } from 'react-feather'
+import { inject, observer } from 'mobx-react';
 
 import PageTitle from '../Pages/PageTitle'
 import PageWrapper from '../Pages/PageWrapper'
 
 import { capitalizeFirst, removeUnderscore } from '../../helpers/typography'
 
+@inject('CreateStore')
+@observer
 export default class LyricSelect extends Component {
     state = {
-        lyrics: ''
+        lyrics: this.props.CreateStore.lyrics,
+        selectedWords: this.props.CreateStore.selectedWords
     }
 
     componentDidMount() {
-        let that = this;
+        // let that = this;
         this.props.CreateStore.getLyrics()
-            .then(res => {
-                that.getLyricsDOM(res)
-            })
-            .catch(e => console.error(e))
+        //     .then(res => {
+        //         that.getLyricsDOM(res)
+        //     })
+        //     .catch(e => console.error(e))
     }
 
-    getLyricsDOM(lyrics = this.state.lyrics) {
+    componentWillReceiveProps(nextProps) {
+        console.log(nextProps)
+    }
+
+    getLyricsDOM() {
         let that = this;
+        let lyrics = this.props.CreateStore.lyrics
 
         if (lyrics.trim() !== '') {
             lyrics = JSON.stringify(lyrics)
@@ -30,14 +40,28 @@ export default class LyricSelect extends Component {
             wrapped = wrapped.replace(/['?"()\\]/g, '');
             
             wrapped = wrapped.replace(word_pattern, function(p1, p2) {
-                let i = that.props.CreateStore.selectedWords.indexOf(p1.toLowerCase().trim());
+                let i = that.state.selectedWords.indexOf(p1.toLowerCase().trim());
                 return `${p2}<span class="${ i !== -1 ? `active no-${i}` : '' }">${p1}</span>`
             })
-            
-            this.setState({
-                lyrics: wrapped
-            })
+
+            return wrapped
         }
+    }
+
+    renderWordPills() {
+        let words = this.state.selectedWords;
+        if (words.length === 0) {
+            return false
+        }
+
+        let pills = words.map((word, key) => (
+            <li key={key} className={`pill no-${key}`} onClick={() => this.props.CreateStore.removeWord(key)}>
+                {word}
+                <X />
+            </li>
+        ))
+
+        return <ul className="selected-words">{pills}</ul>
     }
 
     render() {
@@ -48,7 +72,7 @@ export default class LyricSelect extends Component {
                     <span className="artist c-caption">{capitalizeFirst(CreateStore.selectedArtist)}</span>
                 </PageTitle>
                 <div className="lyric-select-wrapper">
-                    <div className="lyric-wrapper" dangerouslySetInnerHTML={{ __html: this.state.lyrics }} />
+                    <div className="lyric-wrapper" dangerouslySetInnerHTML={{ __html: this.getLyricsDOM() }} />
                     <div className="lyric-data">
                         <span className="c-caption">Word Selection</span>
                         {
@@ -58,6 +82,7 @@ export default class LyricSelect extends Component {
                                 <h5 className="user-direction">Select Another Word - {CreateStore.selectedWords.length}/5</h5>
                             )
                         }
+                        { this.renderWordPills() }
                     </div>
                 </div>
             </PageWrapper>
