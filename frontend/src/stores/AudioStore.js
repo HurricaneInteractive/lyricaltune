b7f69732c756c0fe8c55f7bbfa10ceb205e80773
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-15 17:01:24 +1100

    Updated stop function / beat index for visual indicator on FE

--- frontend/src/stores/AudioStore.js
+++ frontend/src/stores/AudioStore.js
@@ -7,10 +7,16 @@ class AudioStore {
     synth = new Tone.Synth().toMaster()
     loop = null
     Transport = Tone.Transport
+    note_fraction = '16n'
+    @observable currentBeat = 0;
     @observable playing = false
 
     @computed get isPlaying() {
         return this.playing
+    }
+
+    @computed get beatIndex() {
+        return this.currentBeat
     }
 
     chordSequence(chords) {
@@ -35,16 +41,22 @@ class AudioStore {
 
     @action
     stopTransportLoop() {
-        this.Transport.stop();
+        console.log('stop');
+        this.Transport.stop()
+        this.Transport.cancel()
+
         runInAction(() => {
+            this.Transport = Tone.Transport
             this.playing = false
+            this.currentBeat = 0
         })
     }
 
     @action
     transportLoop(chords) {
-        let chords_array = this.chordSequence(chords),
-            i = 0
+        console.log('start')
+        // Tone.Master.volumn = 0.5;
+        let chords_array = this.chordSequence(chords)
         
         runInAction(() => {
             this.playing = true
@@ -53,26 +65,31 @@ class AudioStore {
         this.Transport.scheduleRepeat((time) => {
             let ticks = this.Transport.ticks
             if (ticks > 0 && ticks < 20) {
-                i = 0;
+                runInAction(() => {
+                    this.currentBeat = 0
+                })
             }
 
-            if (typeof chords_array[i] === 'undefined') {
+            if (typeof chords_array[this.beatIndex] === 'undefined') {
                 this.stopTransportLoop();
                 return false;
             }
 
-            let chord = chords_array[i]
+            let chord = chords_array[this.beatIndex]
             if (chord.length !== 0) {
                 let polySynth = new Tone.PolySynth(chord.length, Tone.Synth).toMaster();
-                polySynth.triggerAttackRelease(chord, "16n");
+                polySynth.triggerAttackRelease(chord, this.note_fraction);
             }
-            i++
-        }, "16n", 0, Tone.Time("16n") * 32)
+
+            runInAction(() => {
+                this.currentBeat += 1
+            })
+        }, this.note_fraction, 0, Tone.Time(this.note_fraction) * 32)
 
         this.Transport.loop = true
         this.Transport.loopStart = 0
-        this.Transport.loopEnd = Tone.Time('16n') * 32
-        this.Transport.bpm.value = 80
+        this.Transport.loopEnd = Tone.Time(this.note_fraction) * 32
+        this.Transport.bpm.value = 180
 
         this.Transport.start('+0.1')
     }
