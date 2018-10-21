8c2c17c6ed12ae7040bce0fb00ecd91cc689d910
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-21 11:16:21 +1100

    Ability to add classnames to form group

--- frontend/src/components/Form/FormGroup.js
+++ frontend/src/components/Form/FormGroup.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { removeUnderscore } from '../../helpers/typography'
 
 const FormGroup = (props) => (
-    <div className="input-group">
+    <div className={`input-group ${ props.classnames ? props.classnames : '' }`}>
         <label htmlFor={props.label}>{removeUnderscore(props.label)}</label>
         {props.children}
     </div>
