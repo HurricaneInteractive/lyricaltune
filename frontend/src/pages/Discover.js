e2164cbcc9add87f89c70fc1d2aa65d6942fb416
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-21 14:57:37 +1100

    Updated discover page structure

--- frontend/src/pages/Discover.js
+++ frontend/src/pages/Discover.js
@@ -5,13 +5,37 @@ import PageTitle from '../components/Pages/PageTitle'
 
 import SearchForm from '../components/WebForms/Search'
 
+import { performAxiosCall } from '../helpers/api'
+
+import PhraseCollection from '../components/Phrases/PhraseCollection'
+
 export default class Discover extends Component {
+    state = {
+        phrases: null
+    }
+
+    componentDidMount() {
+        performAxiosCall('/phrases/')
+            .then(phrases => {
+                this.setState({
+                    phrases: phrases.data.phrases
+                })
+            })
+            .catch(e => console.error(e))
+    }
+
     render() {
+        let { phrases } = this.state
+        let { UserStore, AudioStore } = this.props
+
         return (
             <PageWrapper>
                 <PageTitle title="Discover">
                     <SearchForm />
                 </PageTitle>
+                <div className="page-content">
+                    { phrases ? <PhraseCollection UserStore={UserStore} AudioStore={AudioStore} phrases={phrases} /> : 'Loading...' }
+                </div>
             </PageWrapper>
         )
     }
