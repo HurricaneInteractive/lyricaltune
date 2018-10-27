11b241941d238246e3123cc9c3496cb88593b8e7
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-27 14:27:21 +1100

    Added profile image

--- frontend/src/components/Pages/Profile/ProfileHeader.js
+++ frontend/src/components/Pages/Profile/ProfileHeader.js
@@ -1,13 +1,14 @@
 import React, { PureComponent } from 'react'
 
 import ProfileWebsites from './ProfileWebsites'
+import profileImage from '../../../images/profile.jpg'
 
 export default class ProfileHeader extends PureComponent {
     render() {
         let { user } = this.props
         return (
             <div className="profile-header">
-                <div className="profile-image"></div>
+                <div className="profile-image" style={{ backgroundImage: `url('${profileImage}')` }}></div>
                 <h4>{ user.name }</h4>
                 <h1>@{ user.username }</h1>
                 <ProfileWebsites websites={user.websites} />
