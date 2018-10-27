8034a138895709ccb3a21d61d3b756ad7056e8f7
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-27 14:28:08 +1100

    Register modal with override submit function

--- frontend/src/components/Navigation/LoginSignup.js
+++ frontend/src/components/Navigation/LoginSignup.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 import Login from '../WebForms/Login'
+import Register from '../WebForms/Register'
 import Modal from '../Modal'
 
 export default class LoginSignup extends Component {
@@ -18,7 +19,8 @@ export default class LoginSignup extends Component {
         e.preventDefault()
         let currentState = this.state[key]
         this.setState({
-            [key]: !currentState
+            [key]: !currentState,
+            [key === 'loginOpen' ? 'registerOpen' : 'loginOpen']: false
         })
     }
 
@@ -30,6 +32,15 @@ export default class LoginSignup extends Component {
                 }
             })
             .catch(e => console.error('Login Error', e))
+    }
+
+    registerUser = (values) => {
+        // console.log('Register', values);
+        this.props.UserStore.registerUser(values)
+            .then(res => {
+                // console.log(res)
+            })
+            .catch(e => console.error('Register Error', e))
     }
 
     render() {
@@ -45,13 +56,22 @@ export default class LoginSignup extends Component {
                         </Modal>
                     ) : ('')
                 }
+                {
+                    this.state.registerOpen ? (
+                        <Modal close={(e) => this.triggerModal(e, 'registerOpen')}>
+                            <h2 className="text--center c-dark-blue">Register</h2>
+                            <Register overrideSubmit={(values) => this.registerUser(values)} GlobalStore={this.props.GlobalStore} />
+                        </Modal>
+                    ) : ('')
+                }
                 <li><a href="#login-modal" onClick={(e) => this.triggerModal(e, 'loginOpen')}>Log In</a></li>
-                <li><a href="#register-modal">Sign Up</a></li>
+                <li><a href="#register-modal" onClick={(e) => this.triggerModal(e, 'registerOpen')}>Sign Up</a></li>
             </Fragment>
         )
     }
 }
 
 LoginSignup.propTypes = {
-    UserStore: PropTypes.any.isRequired
+    UserStore: PropTypes.any.isRequired,
+    GlobalStore: PropTypes.any.isRequired
 }
