6960e30864ee9bc77d286648a3e1d6c385e42394
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-29 13:47:40 +1100

    added classnames prop

--- frontend/src/components/Pages/PageWrapper.js
+++ frontend/src/components/Pages/PageWrapper.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default class PageWrapper extends PureComponent {
     render() {
         return (
-            <main className="page-wrapper">
+            <main className={`page-wrapper ${this.props.classnames}`}>
                 <div className="container">
                     {this.props.children}
                 </div>
@@ -14,5 +14,10 @@ export default class PageWrapper extends PureComponent {
 }
 
 PageWrapper.propTypes = {
-    children: PropTypes.node.isRequired
+    children: PropTypes.node.isRequired,
+    classnames: PropTypes.string
+}
+
+PageWrapper.defaultProps = {
+    classnames: ''
 }
