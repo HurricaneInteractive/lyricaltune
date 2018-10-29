4fd80c18cf657cb10d08b396d7135392fbf4679b
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-10-29 18:39:05 +1100

    Scroll to top on change component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import {
 
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 
 import Home from './pages/Home'
 import About from './pages/About'
@@ -40,57 +41,59 @@ class App extends Component {
 		console.log(UserStore.current_user)
 		return (
 			<Router>
-				<div className="App">
-					{ 
-						GlobalStore.second_chance !== false ? (
-							<SecondChanceAuth UserStore={UserStore} GlobalStore={GlobalStore} second_chance={GlobalStore.second_chance} /> 
-						) : ('')
-					}
-					{
-						GlobalStore.response_error !== null ? (
-							<ErrorPopup errors={GlobalStore.errors} dismiss={() => GlobalStore.dismissResponseError()} />
-						) : ('')
-					}
-					<Route render={(routerProps) => (
-						<Header
+				<ScrollToTop>
+					<div className="App">
+						{ 
+							GlobalStore.second_chance !== false ? (
+								<SecondChanceAuth UserStore={UserStore} GlobalStore={GlobalStore} second_chance={GlobalStore.second_chance} /> 
+							) : ('')
+						}
+						{
+							GlobalStore.response_error !== null ? (
+								<ErrorPopup errors={GlobalStore.errors} dismiss={() => GlobalStore.dismissResponseError()} />
+							) : ('')
+						}
+						<Route render={(routerProps) => (
+							<Header
+								authenticated={ UserStore.current_user === null ? false : true }
+								UserStore={UserStore}
+								routerProps={routerProps}
+								GlobalStore={GlobalStore}
+							/>
+						)} />
+						
+						<Route exact path="/" component={Home} />
+						<Route path="/about" component={About} />
+						<Route path="/discover" render={() => (
+							<Discover
+								UserStore={UserStore}
+								AudioStore={AudioStore}
+							/>
+						)} />
+						<Route path="/create" render={(routerProps) => {
+							return <Create
+								routerProps={routerProps}
+								authenticated={ UserStore.current_user === null ? false : true }
+								UserStore={UserStore}
+								CreateStore={CreateStore}
+								AudioStore={AudioStore}
+							/>
+						}} />
+						<Route path="/profile/:id" render={(routerProps) => (
+							<Profile
+								routerProps={routerProps}
+								UserStore={UserStore}
+								current_user={UserStore.current_user}
+							/>
+						)} />
+
+						<Footer
 							authenticated={ UserStore.current_user === null ? false : true }
 							UserStore={UserStore}
-							routerProps={routerProps}
 							GlobalStore={GlobalStore}
 						/>
-					)} />
-					
-					<Route exact path="/" component={Home} />
-					<Route path="/about" component={About} />
-					<Route path="/discover" render={() => (
-						<Discover
-							UserStore={UserStore}
-							AudioStore={AudioStore}
-						/>
-					)} />
-					<Route path="/create" render={(routerProps) => {
-						return <Create
-							routerProps={routerProps}
-							authenticated={ UserStore.current_user === null ? false : true }
-							UserStore={UserStore}
-							CreateStore={CreateStore}
-							AudioStore={AudioStore}
-						/>
-					}} />
-					<Route path="/profile/:id" render={(routerProps) => (
-						<Profile
-							routerProps={routerProps}
-							UserStore={UserStore}
-							current_user={UserStore.current_user}
-						/>
-					)} />
-
-					<Footer
-						authenticated={ UserStore.current_user === null ? false : true }
-						UserStore={UserStore}
-						GlobalStore={GlobalStore}
-					/>
-				</div>
+					</div>
+				</ScrollToTop>
 			</Router>
 		);
 	}
